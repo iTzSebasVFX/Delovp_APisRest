83559b135974b3dbc789a6bb6b46b9733b61d9fd
iyer smith torres <torresjaramilloiyersmith@gmail.com>
2025-05-15 14:52:56 -0500

    actualizacion de archivos

---
+++ ProyectosPython/FLASK_MYSQL_PROYECTO/db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE flask_db;
+
+USE flask_db;
+  --tabla persona---
+
+CREATE Table personas(
+id INT AUTO_INCREMNT PRIMARY KEY,
+nombre VARCHAR(100)
+  )
---
+++ ProyectosPython/clase4modulosYPaquetesejemplo/my_paquet/operacion_texto.py
@@ -0,0 +1,27 @@
+#
+
+
+frase = 'holaaa buenos dias a todo america'
+
+frase = 'desea ingresar una suma '
+
+def suma(a,b):
+    return a + b
+
+matematicas = 'desea ingresar una multiplicacion'
+
+def multiplicacion(a,b):
+     return a *b
+
+def divicion(a,b):
+    return a / b
+
+
+
+
+
+
+
+    
+
+ 
